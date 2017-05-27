b539d088aa336c0126e859dff9ccfc2dfe411b5b
twilroad <269044570@qq.com>
2017-05-27 19:58:25 +0800

    add permission group register

--- src/ModuleServiceProvider.php
+++ src/ModuleServiceProvider.php
@@ -12,6 +12,7 @@ use Illuminate\Events\Dispatcher;
 use Notadd\Foundation\Module\Abstracts\Module;
 use Notadd\Wechat\Injections\Installer;
 use Notadd\Wechat\Injections\Uninstaller;
+use Notadd\Wechat\Listeners\PermissionGroupRegister;
 use Notadd\Wechat\Listeners\PermissionTypeRegister;
 
 /**
@@ -24,6 +25,7 @@ class ModuleServiceProvider extends Module
      */
     public function boot()
     {
+        $this->app->make(Dispatcher::class)->subscribe(PermissionGroupRegister::class);
         $this->app->make(Dispatcher::class)->subscribe(PermissionTypeRegister::class);
     }
 
